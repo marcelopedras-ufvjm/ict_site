bcb3431be8fea0baebc2890e8279feaa7b8a0fbb
marcelo <marcelo.pedras@ict.ufvjm.edu.br>
2015-05-15 13:31:29 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IctSite::Application.config.secret_token = '902e5a8bbc74ffdcd4c9d18917c1c43c12ffdc221d525295d00d69c35a39f5b9c8f4efd40a85542aff3f3399fc546bb41704ba5fc0e6ff5a2d584ae2cf5b0c20'
